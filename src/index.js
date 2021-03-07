77da6773880d66b9613ea5a9dc418b584906a65f
Damian <damiandanielvigo@gmail.com>
2021-03-07 15:34:37 -0300

    add-routes-layout-finish

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // settings
-app.set('port', 4000);
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile); // cambio extension ejs a html
+app.set('view engine', 'ejs');
 
 // middlewares
 
-
 // routes
-app.get('/', (req, res) => {
-  res.send('Hey');
-})
+app.use(require('./routes/index'));
 
 // static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // listening the server
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
-})
+});
